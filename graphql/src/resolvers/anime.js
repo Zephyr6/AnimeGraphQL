b571c5c5761179e7ff77863c28dca365478f1fcc
Carlos Cruz <carlos-cruz@pluralsight.com>
2018-03-11 14:03:57 -0600

    Add some comments to the anime resolver

--- graphql/src/resolvers/anime.js
+++ graphql/src/resolvers/anime.js
@@ -8,21 +8,24 @@ const baseUrl = config.api.mikan.baseUrl
 export default async function anime(root, args) {
   let { id } = args
 
+  // Try to get a cached anime before sending a request to the external API
   let cachedAnime = await db.findAnime(id)
   if (cachedAnime) {
     console.log('Returning cached anime: ' + cachedAnime.title)
     return cachedAnime
   }
 
+  // No cached anime found, make request
   console.log('Requesting anime ' + id)
   return axios
     .get(baseUrl + '/anime/' + id)
     .then(response => {
+      // Anime found, cache it
       db.insertAnime(response.data)
+
       return response.data
     })
     .catch(err => {
-      db.deleteAnime(id)
       console.error(err.message)
       return null
     })
